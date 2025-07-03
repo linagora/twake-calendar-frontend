d6ff1b4ec7a675378c4122e2bc2df52f8f413dcb
Camille Moussu <cmoussu@linagora.com>
2025-07-03 16:49:44 +0200

    Added cozy-ui and needed dependencies to project

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,24 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./app/store";
-
+import { BreakpointsProvider } from "cozy-ui/transpiled/react/providers/Breakpoints";
+import CozyTheme from "cozy-ui/transpiled/react/providers/CozyTheme";
+import { I18n } from "cozy-ui/transpiled/react/providers/I18n";
 import reportWebVitals from "./reportWebVitals";
 const container = document.getElementById("root")!;
 
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <I18n>
+      <CozyTheme ignoreCozySettings={undefined}>
+        <BreakpointsProvider>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </BreakpointsProvider>
+      </CozyTheme>
+    </I18n>
   </React.StrictMode>
 );
 
