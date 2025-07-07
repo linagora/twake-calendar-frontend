5ebdc7d9c9cfec1db6dd4e262d635ba1594fe850
Théo Poizat <hello@zatteo.com>
2025-07-07 17:13:58 +0200

    WIP: Try cozy-ui setup
    - Added pluginNodePolyfill to fix build issues with rsbuild and cozy-ui about process.env
    - Trying to initialize properly I18n provider
    
    Not sure if downgrading React to 18 is necessary. Officialy, only MUI >= 5 is compatible with React 19 but I need to dig further if it is an issue for us or not.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+import 'cozy-ui/transpiled/react/stylesheet.css';
+import 'cozy-ui/dist/cozy-ui.utils.min.css';
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
@@ -6,13 +9,19 @@ import { store } from "./app/store";
 import { BreakpointsProvider } from "cozy-ui/transpiled/react/providers/Breakpoints";
 import CozyTheme from "cozy-ui/transpiled/react/providers/CozyTheme";
 import { I18n } from "cozy-ui/transpiled/react/providers/I18n";
+import { initTranslation } from 'cozy-ui/transpiled/react/providers/I18n'
 import reportWebVitals from "./reportWebVitals";
 const container = document.getElementById("root")!;
 
 const root = createRoot(container);
+
+const locale = 'fr';
+
+const polyglot = initTranslation(locale, lang => {})
+
 root.render(
   <React.StrictMode>
-    <I18n>
+    <I18n lang={locale} polyglot={polyglot}>
       <CozyTheme ignoreCozySettings={undefined}>
         <BreakpointsProvider>
           <Provider store={store}>
