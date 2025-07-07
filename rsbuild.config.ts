eefad76d88e0f1073b2f87e820502cf51585ac06
Théo Poizat <hello@zatteo.com>
2025-07-07 17:13:58 +0200

    WIP: Try cozy-ui setup
    - Added pluginNodePolyfill to fix build issues with rsbuild and cozy-ui about process.env
    - Trying to initialize properly I18n provider
    
    Not sure if downgrading React to 18 is necessary. Officialy, only MUI >= 5 is compatible with React 19 but I need to dig further if it is an issue for us or not.

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill';
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(), pluginNodePolyfill(),
+],
   html: {
     template: "./public/index.html",
   },
