2ad41dc4f5d4935c836af76d2dc7b8e9517610d1
Camille Moussu <cmoussu@linagora.com>
2025-07-25 15:29:40 +0200

    [#3] added event deletion to display

--- src/features/Events/EventModal.tsx
+++ src/features/Events/EventModal.tsx
@@ -70,7 +70,7 @@ function EventPopover({
 
   const handleSave = async () => {
     const newEvent: CalendarEvent = {
-      calId: calendarid,
+      calId: userPersonnalCalendars[calendarid].id,
       title,
       start: new Date(start),
       allday,
