51d3acf24e98708295268797792295eebb5f40e2
Camille Moussu <cmoussu@linagora.com>
2025-08-18 18:10:28 +0200

    [#32] added app list and user icon to menubar

---
+++ public/appList.example.js
@@ -0,0 +1,7 @@
+var appList = [
+  {
+    name: "Twake",
+    link: "/twake",
+    icon: "https://twake-chat.com/tild3037-6365-4134-b838-383030366263__twake-chat-logo.svg",
+  },
+];
--- src/components/Menubar/Menubar.tsx
+++ src/components/Menubar/Menubar.tsx
@@ -1,17 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../static/images/calendar.svg";
+import AppsIcon from "@mui/icons-material/Apps";
 import "./Menubar.css";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { stringToColor } from "../../features/Events/EventDisplay";
+import { Avatar, IconButton, Popover } from "@mui/material";
+import { push } from "redux-first-history";
+
+export type AppIconProps = {
+  name: string;
+  link: string;
+  icon: string;
+};
+
 export function Menubar() {
+  const user = useAppSelector((state) => state.user.userData);
+  const applist: AppIconProps[] = (window as any).appList;
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const dispatch = useAppDispatch();
+
+  if (!user) {
+    dispatch(push("/"));
+  }
+  const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
   return (
-    <header className="menubar">
-      <MainTitle/>
-      <div className="menubar-item search-bar">
-        <p>big search bar</p>
-      </div>
-      <div className="menubar-item">
-        <p>Account stuff</p>
-      </div>
-    </header>
+    <>
+      <header className="menubar">
+        <MainTitle />
+        <div className="menubar-item search-bar">
+          <p>big search bar</p>
+        </div>
+
+        <div className="menubar-item">
+          {applist.length > 0 && (
+            <IconButton onClick={handleOpen}>
+              <AppsIcon />
+            </IconButton>
+          )}
+          <Avatar
+            sx={{ bgcolor: stringToColor(user.family_name) }}
+            sizes="large"
+          >
+            {user.name[0]}
+            {user.family_name[0]}
+          </Avatar>
+        </div>
+      </header>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <div className="app-grid">
+          {applist.map((prop: AppIconProps) => (
+            <AppIcon prop={prop} />
+          ))}
+        </div>
+      </Popover>
+    </>
   );
 }
 export function MainTitle() {
@@ -23,5 +85,20 @@ export function MainTitle() {
         <span className="calendar-text">Calendar</span>
       </p>
     </div>
+  );
+}
+
+function AppIcon({ prop }: { prop: AppIconProps }) {
+  return (
+    <a
+      key={prop.name}
+      href={prop.link}
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <div>
+        <img src={prop.icon} alt={prop.name} />
+        <p>{prop.name}</p>
+      </div>
+    </a>
   );
 }
