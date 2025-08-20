59c977a3b3e78f1ad02e9cce30d7b6f7c14c78fa
Camille Moussu <cmoussu@linagora.com>
2025-08-20 10:14:47 +0200

    [#32] changed how userinfo is filled

--- src/features/User/userAPI.ts
+++ src/features/User/userAPI.ts
@@ -1,6 +1,6 @@
 import { api } from "../../utils/apiUtils";
 
-export default async function getOpenPaasUserId() {
+export default async function getOpenPaasUser() {
   const user = await api.get(`api/user`).json();
   return user;
 }
