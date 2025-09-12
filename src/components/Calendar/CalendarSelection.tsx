5717eb44ebcee50e0c974711e0452d8f0101ac7c
Camille Moussu <cmoussu@linagora.com>
2025-09-12 09:31:34 +0200

    [#64] fixed css and empty name bug

--- src/components/Calendar/CalendarSelection.tsx
+++ src/components/Calendar/CalendarSelection.tsx
@@ -28,7 +28,6 @@ export default function CalendarSelection({
     );
   };
 
-  useEffect(() => {}, [calendars]);
   const [anchorElCal, setAnchorElCal] = useState<HTMLElement | null>(null);
 
   return (
