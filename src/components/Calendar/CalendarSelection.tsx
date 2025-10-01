b2b786e3cee9e4baf2b1062f802f1856d4ea8e0b
lenhanphung <lenhanphung@gmail.com>
2025-10-01 19:31:09 +0700

    resolve conflict

--- src/components/Calendar/CalendarSelection.tsx
+++ src/components/Calendar/CalendarSelection.tsx
@@ -215,7 +215,6 @@ function CalendarSelector({
     handleClose();
   };
   return (
-<<<<<<< HEAD
     <>
       <div>
         <label>
@@ -253,23 +252,5 @@ function CalendarSelector({
         handleDeleteConfirm={handleDeleteConfirm}
       />
     </>
-=======
-    <div key={id}>
-      <label>
-        <Checkbox
-          style={{
-            color: calendars[id].color,
-          }}
-          size="small"
-          checked={selectedCalendars.includes(id)}
-          onChange={() => handleCalendarToggle(id)}
-        />
-        {calendars[id].name}
-      </label>
-      <IconButton onClick={() => setOpen()}>
-        <MoreVertIcon />
-      </IconButton>
-    </div>
->>>>>>> a7e29ce (feat: prepare project for cozy-ui migration)
   );
 }
