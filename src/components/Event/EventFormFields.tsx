128d625f21dbdd82cc1163f5ba65568cd4231c71
Camille Moussu <cmoussu@linagora.com>
2025-11-06 16:46:43 +0100

    fixup! [#230] added toast to copylink

--- src/components/Event/EventFormFields.tsx
+++ src/components/Event/EventFormFields.tsx
@@ -759,7 +759,7 @@ export default function EventFormFields({
       <SnackbarAlert
         setOpen={setOpenToast}
         open={openToast}
-        message={t("event.meetCopied")}
+        message={t("event.form.meetCopied")}
       />
     </>
   );
