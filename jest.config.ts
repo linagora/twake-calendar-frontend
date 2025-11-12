f76554f8ea856bfccdad0a4d5d9177ffcbe3a161
Camille Moussu <66134347+Eriikah@users.noreply.github.com>
2025-11-12 09:52:20 +0100

    [#179] functionnal topbar when dialog expanded (#292)
    Co-authored-by: Camille Moussu <cmoussu@linagora.com>

--- jest.config.ts
+++ jest.config.ts
@@ -26,7 +26,8 @@ const config: Config = {
       transform: {
         "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
         "^.+\\.(js|jsx|mjs)$": "babel-jest",
-        "^.+\\.(css|scss|sass|less)$": "jest-preview/transforms/css",
+        "^.+\\.(css|scss|sass|less|styl|stylus)$":
+          "jest-preview/transforms/css",
         "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)":
           "jest-preview/transforms/file",
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
