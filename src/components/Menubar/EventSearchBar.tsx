506e873bdea7fc61be170c93c3f5f749a416b140
Camille Moussu <cmoussu@linagora.com>
2025-12-08 09:56:03 +0100

    [#394] blocking search on enter when search field is empty and when keyword is empty in filters modal

--- src/components/Menubar/EventSearchBar.tsx
+++ src/components/Menubar/EventSearchBar.tsx
@@ -84,6 +84,16 @@ export default function SearchBar() {
   };
 
   const handleSearch = async () => {
+    const trimmedSearch = search.trim();
+    const trimmedKeywords = filters.keywords.trim();
+    if (!filterOpen && !trimmedSearch) {
+      return;
+    }
+
+    if (filterOpen && !trimmedKeywords) {
+      return;
+    }
+
     let searchInCalendars: string[];
 
     if (filters.searchIn === "" || !filters.searchIn) {
@@ -97,7 +107,7 @@ export default function SearchBar() {
     }
 
     const cleanedFilters = {
-      ...filters,
+      keywords: trimmedKeywords,
       organizers: filters.organizers.map((u) => u.cal_address),
       attendees: filters.attendees.map((u) => u.cal_address),
       searchIn: searchInCalendars,
@@ -105,7 +115,7 @@ export default function SearchBar() {
 
     dispatch(
       searchEventsAsync({
-        search,
+        search: trimmedSearch,
         filters: cleanedFilters,
       })
     );
