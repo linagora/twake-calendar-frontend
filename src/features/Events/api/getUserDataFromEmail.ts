cf6f98f1a9a7a3dfe059a2ebe2acb1253a68c975
Camille Moussu <66134347+Eriikah@users.noreply.github.com>
2026-03-25 12:43:59 +0100

    [#706] skip malformed email adresses in get userdatafromEmail (#712)
    Co-authored-by: Camille Moussu <cmoussu@linagora.com>

--- src/features/Events/api/getUserDataFromEmail.ts
+++ src/features/Events/api/getUserDataFromEmail.ts
@@ -1,6 +1,10 @@
 import { api } from "@/utils/apiUtils";
+import { isValidEmail } from "@/utils/isValidEmail";
 
 export async function getUserDataFromEmail(email: string) {
+  if (!isValidEmail(email)) {
+    return [];
+  }
   const r = await api(`api/users?email=${encodeURIComponent(email)}`);
   const result: Array<Record<string, string>> = await r.json();
   return result;
