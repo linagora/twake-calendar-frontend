ab3fc2c9682c8602441f037bb859311a440da68f
Camille Moussu <cmoussu@linagora.com>
2026-04-15 10:36:44 +0200

    [#786] fully disable long press

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -449,7 +449,7 @@ const CalendarApp: React.FC<CalendarAppProps> = ({
             timeZone={timezone}
             height="100%"
             select={eventHandlers.handleDateSelect}
-            longPressDelay={100}
+            selectLongPressDelay={0}
             nowIndicator
             slotLabelClassNames={arg => [
               updateSlotLabelVisibility(new Date(), arg, timezone)
